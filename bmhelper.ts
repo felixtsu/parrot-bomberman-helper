decf1eb33c15ccc5696017f04e504a0080957920
felixtsu <humaeks@gmail.com>
2020-12-24 15:32:35 +0800

    Update bmhelper.ts

--- bmhelper.ts
+++ bmhelper.ts
@@ -1,7 +1,7 @@
-// Add your code here
-namespace bmHelper {
+namespace bmhelper {
 
-    //%block="move %sprite with smooth alignment"
+    //%block="move %sprite with smooth alignment||vx %vx vy %vy"
+    //% vx.defl=100 vy.defl=100
     //%blockid=bmhelpermovesprite 
     export function moveSprite(sprite:Sprite, vx:number=50, vy:number=50) {
         smoothturn.moveSprite(sprite, vx, vy)
